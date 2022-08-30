84a11aa8e747565995069436f1c6ca60293a4807
+ <+>
2022-08-30 07:35:58 +0300

    commit tree 16edd564fa84eb28d8b25d43cc676c8ccd36ca40

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+    attr_reader :song
+    attr_accessor :name,:artist,:genre
+   
+    def initialize(name,artist,genre)
+        @name =name
+        @artist=artist
+        @genre = genre
+    end
+    @@count=0
+    def count
+        @@count
+        puts count+=1
+    end
+    @@genre
+    genre=[]
+    def genre
+        @@genre
+        puts genre do |g|
+        end
+        artist=[]
+        def Song.artist
+            @@artist
+        end
+end
+end
